57bd8c4096aa6539be5bc5e2407af6a9068b3126
Ira MELESHEV <imeleshev@dom.prodware.fr>
2016-08-02 14:52:58 +0300

    new classes in json

---
+++ app/common/services/data/data-service-spec.js
@@ -0,0 +1,55 @@
+﻿(function () {
+    'use strict';
+
+    fdescribe('dataService', function () {
+
+        var dataService, $httpBackend;
+
+        beforeEach(function () {
+            module('common');
+
+            inject(function (_dataService_, _$httpBackend_) {
+                dataService = _dataService_;
+                $httpBackend = _$httpBackend_;
+            });
+        });
+        
+        afterEach(function(){
+           $httpBackend.verifyNoOutstandingExpectation();
+           $httpBackend.verifyNoOutstandingRequest(); 
+        });
+     
+        /**
+         * test API (Public Interface)
+         */
+        describe("API", function(){
+           
+            it('has get', function () {
+                expect(dataService.get).toBeDefined();
+            });
+            
+        });
+        
+            
+        it('/myUrl - return json object', function () {
+
+            var request = '/myUrl';
+            var response = {"name": "Erez"};
+            $httpBackend.expectGET(request).respond(response);
+            
+            var resultPromise = dataService.get(request);
+            
+            var result;
+            resultPromise.then(function(response){
+               result = response; 
+            });
+            
+            $httpBackend.flush();
+            
+            expect(result).toEqual(response);
+
+        });
+
+    });
+
+})();
---
+++ app/common/services/data/data-service.js
@@ -0,0 +1,181 @@
+﻿(function () {
+    'use strict';
+
+    /* JAVASCRIPT */
+
+    /**
+     * DataService Object/function
+     */
+    function DataService($log, $q, $http,PubSub, ENV) {
+
+        $log = $log.getInstance('DataService', true);
+        $log.debug("load()");
+
+        /***************** PRIVATE *******************/
+
+
+        /**
+         * sendRequest() - wraps call to $http
+         */
+        function _sendRequest(req) {
+            
+            var defer = $q.defer();
+            
+            $log.debug("sendRequest()", req);
+            
+            $http(req)
+                .then(function successCallback(response) {
+                    $log.debug("response = ", response);
+                    if (response.status >= 200 && response.status < 300) {
+                        defer.resolve(response.data);
+                    }
+                    else{
+                        $log.warn("status = ", response.status);
+                        if (response.status === 401) {
+                            PubSub.publish('message-show', { 'msg': 'העבודה מול המערכת עומדת להסתיים הנך מועבר לדף התחברות מחדש' });
+                        }
+                        defer.reject(null);
+                    }
+                }, function errorCallback(response) {
+                    $log.error("response = ", response);
+                    defer.reject(null);
+                });
+                
+            return defer.promise;
+        }
+
+
+        
+        function _get(url) {
+
+            $log.debug("get(" + url + ")");
+
+            var req = {
+                method: 'GET',
+                url: url
+            };
+
+                req.headers = {};
+                $.extend(req.headers, ENV.apiHeadersConf);
+
+            return _sendRequest(req);
+        }
+
+      
+        function _post(url, data, headers) {
+
+            $log.debug("post(" + url + ")");
+
+            var req = {
+                method: 'POST',
+                url: url,
+                headers:headers,
+                data: data
+            };
+
+            if (req.headers) {
+                $.extend(req.headers, ENV.apiHeadersConf);
+            }
+            else {
+                req.headers = {};
+                $.extend(req.headers, ENV.apiHeadersConf);
+            }
+
+            
+            return _sendRequest(req);
+
+        }
+
+        function _put(url, data, headers) {
+
+            $log.debug("put(" + url + ")");
+
+            var req = {
+                method: 'PUT',
+                url: url,
+                headers: headers,
+                data: data
+            };
+
+            if (req.headers) {
+                $.extend(req.headers, ENV.apiHeadersConf);
+            }
+            else {
+                req.headers = {};
+                $.extend(req.headers, ENV.apiHeadersConf);
+            }
+
+            return _sendRequest(req);
+
+        }
+
+
+
+        // TODO IN PRODUCTION - REMOVE COMMENTS.
+        /**
+         * GET request() - Private function
+         */
+        //function _get(url , headers) {
+
+        //    $log.debug("get(" + url + ")");
+
+        //    var req = {
+        //        method: 'GET',
+        //        url: url
+        //    };
+
+        //    return _sendRequest(req);
+        //}
+
+        /**
+         * POST request() - Private function
+         */
+        //function _post(url, data, headers) {
+
+        //    $log.debug("post(" + url + ")");
+
+        //    var req = {
+        //        method: 'POST',
+        //        url: url,
+        //        headers:headers,
+        //        data: data
+        //    };
+
+        //    return _sendRequest(req);
+
+        //}
+
+        /**
+         * PUT request() - Private function
+         */
+        //function _put(url, data, headers) {
+
+        //    $log.debug("put(" + url + ")");
+
+        //    var req = {
+        //        method: 'PUT',
+        //        url: url,
+        //        headers: headers,
+        //        data: data
+        //    };
+
+        //    return _sendRequest(req);
+
+        //}
+
+        /****************** PUBLIC *******************/
+        var service = {
+            get: _get,
+            post: _post,
+            put: _put
+        };
+
+        return service;
+    }
+
+    /* ANGULAR */
+    angular
+        .module('common')
+        .factory('dataService', DataService);
+
+})();
